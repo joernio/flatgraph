14cba577793fcea269c4f71d0139e8ae3498a8f2
Bernhard <bernhard@shiftleft.io>
2022-10-18 17:01:38 +0200

    clean up edge indexing; support node deletion; prepare reindexing; add todo list

--- core/src/main/java/io/joern/odb2/GNode.java
+++ core/src/main/java/io/joern/odb2/GNode.java
@@ -1,15 +1,25 @@
 package io.joern.odb2;
 
-/**A GNode represents a node in a graph. It is basically a glorified pointer.*/
-public class GNode implements DNodeOrNode{
+/**
+ * A GNode represents a node in a graph. It is basically a glorified pointer.
+ */
+public class GNode implements DNodeOrNode {
     final public Graph graph;
 
     final public short kindId;
-    final public int seqId;
 
-    public GNode( Graph graph, short kindId, int seqId) {
+    int _seqId;
+
+    //an extra field just for this looks quite wasteful. But we have 2 bytes of alignment padding anyways...
+    boolean _isDeleted = false;
+
+    final public int seqId() {
+        return this._seqId;
+    }
+
+    public GNode(Graph graph, short kindId, int seqId) {
         this.kindId = kindId;
-        this.seqId = seqId;
+        this._seqId = seqId;
         this.graph = graph;
     }
 }
--- core/src/main/scala/io/joern/odb2/Schema.scala
+++ core/src/main/scala/io/joern/odb2/Schema.scala
@@ -3,6 +3,12 @@ package io.joern.odb2
 trait Schema {
   def getNNodeKinds: Int
   def getNEdgeKinds: Int
+
+  def indexEdge(kid: Short, inOut: Int, eid: Short): Int =
+    indexEdge(kid.toInt, inOut, eid.toInt)
+
+  def indexEdge(kid: Int, inOut: Int, eid: Int): Int = 2 * (kid + getNNodeKinds * (inOut + 2 * eid))
+
   def makeNode(g: Graph, kid: Short, seqId: Int): GNode
 
   //fixme: API will need to change when we add generated edge classes (relevant for edge properties)
