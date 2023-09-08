5f32aec350420974a19f27032b818c774344eff4
Michael Pollmeier <michael@michaelpollmeier.com>
2023-09-08 12:28:48 +0200

    follow conventions: classes should match file names

--- benchmarks/src/main/scala/io/joern/joernBench/EntryPoint.scala
+++ benchmarks/src/main/scala/io/joern/joernBench/EntryPoint.scala
@@ -3,9 +3,9 @@ package io.joern.joernBench
 object EntryPoint {
   def main(args: Array[String]): Unit = {
     args(0) match {
-      case "jmh"  => JmhMain.jmhMain()
-      case "odb"  => Bench.benchJoern()
-      case "odb2" => Bench.benchOdb2()
+      case "jmh"  => JmhBenchmarks.jmhMain()
+      case "odb"  => LoadingAndMemoryBenchmarks.benchJoern()
+      case "odb2" => LoadingAndMemoryBenchmarks.benchOdb2()
     }
   }
 
