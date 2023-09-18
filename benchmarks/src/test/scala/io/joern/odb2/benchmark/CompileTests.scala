2fd471fa06f2cd29be3137dc575ac75fcae8a542
Michael Pollmeier <michael@michaelpollmeier.com>
2023-09-18 19:28:07 +0200

    drop `nodes.` type prefix, cleanup imports

--- benchmarks/src/test/scala/io/joern/odb2/benchmark/CompileTests.scala
+++ benchmarks/src/test/scala/io/joern/odb2/benchmark/CompileTests.scala
@@ -1,9 +1,8 @@
 package io.joern.odb2.benchmark
 
-import io.shiftleft.codepropertygraph.generated.v2
-import v2.accessors.Lang.*
-import v2.nodes
-import v2.traversals.Lang.*
+import io.shiftleft.codepropertygraph.generated.v2.accessors.Lang.*
+import io.shiftleft.codepropertygraph.generated.v2.nodes.*
+import io.shiftleft.codepropertygraph.generated.v2.traversals.Lang.*
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -15,7 +14,7 @@ import org.scalatest.wordspec.AnyWordSpec
 class CompileTests extends AnyWordSpec with Matchers {
   import CompileTests.*
 
-  def concreteStored(it: Iterator[nodes.Call]): Unit = {
+  def concreteStored(it: Iterator[Call]): Unit = {
     it.name("a")
     it.order
     it.next.order
@@ -23,7 +22,7 @@ class CompileTests extends AnyWordSpec with Matchers {
     it.next.isStatic.map { _.staticCallee }
   }
 
-  def concreteBase(it: Iterator[nodes.CallBase]): Unit = {
+  def concreteBase(it: Iterator[CallBase]): Unit = {
     it.name("a")
     it.order
     it.next.order
@@ -33,7 +32,7 @@ class CompileTests extends AnyWordSpec with Matchers {
     // error:  it.next.isStatic.map{_.staticCallee}
   }
 
-  def concreteNew(it: Iterator[nodes.NewCall]): Unit = {
+  def concreteNew(it: Iterator[NewCall]): Unit = {
     it.name("a")
     it.order
     it.next.order
@@ -43,19 +42,19 @@ class CompileTests extends AnyWordSpec with Matchers {
     // error:  it.next.isStatic.map{_.staticCallee}
   }
 
-  def abstractStored(it: Iterator[nodes.CallRepr]): Unit = {
+  def abstractStored(it: Iterator[CallRepr]): Unit = {
     it.name("a")
     it.order
     it.next.order
   }
 
-  def abstractBase(it: Iterator[nodes.CallReprBase]): Unit = {
+  def abstractBase(it: Iterator[CallReprBase]): Unit = {
     it.name("a")
     it.order
     it.next.order
   }
 
-  def abstractNew(it: Iterator[nodes.CallReprNew]): Unit = {
+  def abstractNew(it: Iterator[CallReprNew]): Unit = {
     it.name("a")
     it.order
     it.next.name
@@ -63,19 +62,19 @@ class CompileTests extends AnyWordSpec with Matchers {
 
   // here, both abstract types declare "name". So we test that the implicits are disambiguated.
   // there exist no nodes in the intersection, but the code should still compile.
-  def conflictStored(it: Iterator[nodes.Declaration with nodes.CallRepr]): Unit = {
+  def conflictStored(it: Iterator[Declaration with CallRepr]): Unit = {
     it.name("a")
     it.order
     it.next.name
   }
 
-  def conflictBase(it: Iterator[nodes.DeclarationBase with nodes.CallReprBase]): Unit = {
+  def conflictBase(it: Iterator[DeclarationBase with CallReprBase]): Unit = {
     it.name("a")
     it.order
     it.next.name
   }
 
-  def conflictNew(it: Iterator[nodes.DeclarationNew with nodes.CallReprNew]): Unit = {
+  def conflictNew(it: Iterator[DeclarationNew with CallReprNew]): Unit = {
     it.name("a")
     it.order
     it.next.name
@@ -86,23 +85,23 @@ class CompileTests extends AnyWordSpec with Matchers {
 object CompileTests {
   trait IsStaticT
 
-  implicit class IsStaticExt[NodeType <: nodes.CallBase](val node: NodeType) extends AnyVal {
+  implicit class IsStaticExt[NodeType <: CallBase](val node: NodeType) extends AnyVal {
     // n.b. this should really be `def isStatic: Boolean` - the reason it's not is simply that we wanted to have compile-time test for
     // complex cases and didn't really think this through...
-    def isStatic: Option[NodeType with nodes.StaticType[IsStaticT]] =
-      if (node.dispatchType == "STATIC_DISPATCH") Some(node.asInstanceOf[NodeType with nodes.StaticType[IsStaticT]]) else None
+    def isStatic: Option[NodeType with StaticType[IsStaticT]] =
+      if (node.dispatchType == "STATIC_DISPATCH") Some(node.asInstanceOf[NodeType with StaticType[IsStaticT]]) else None
   }
 
-  implicit class IsStaticTravExt[NodeType <: nodes.CallBase](val trav: Iterator[NodeType]) extends AnyVal {
-    def isStatic: Iterator[NodeType with nodes.StaticType[IsStaticT]] = trav.flatMap { _.isStatic }
+  implicit class IsStaticTravExt[NodeType <: CallBase](val trav: Iterator[NodeType]) extends AnyVal {
+    def isStatic: Iterator[NodeType with StaticType[IsStaticT]] = trav.flatMap { _.isStatic }
   }
 
-  implicit class StaticCallExt[NodeType <: nodes.Call with nodes.StaticType[IsStaticT]](val node: NodeType) extends AnyVal {
-    def staticCallee: nodes.Method = node._callOut.head.asInstanceOf[nodes.Method]
+  implicit class StaticCallExt[NodeType <: Call with StaticType[IsStaticT]](val node: NodeType) extends AnyVal {
+    def staticCallee: Method = node._callOut.head.asInstanceOf[Method]
   }
 
-  implicit class StaticCallTravExt[NodeType <: nodes.Call with nodes.StaticType[IsStaticT]](val trav: Iterator[NodeType]) extends AnyVal {
-    def staticCallee: Iterator[nodes.Method] = trav.map { _.staticCallee }
+  implicit class StaticCallTravExt[NodeType <: Call with StaticType[IsStaticT]](val trav: Iterator[NodeType]) extends AnyVal {
+    def staticCallee: Iterator[Method] = trav.map { _.staticCallee }
   }
 
 
