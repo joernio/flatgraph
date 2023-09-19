24402e7247a1ec260abba129adbe69b29de79a94
Michael Pollmeier <michael@michaelpollmeier.com>
2023-09-19 08:47:22 +0200

    internal helper classes: privatise and move to the fineprint area

--- core/src/main/scala/io/joern/odb2/DiffGraphBuilder.scala
+++ core/src/main/scala/io/joern/odb2/DiffGraphBuilder.scala
@@ -5,23 +5,6 @@ import io.joern.odb2.Edge.Direction.{Incoming, Outgoing}
 
 import scala.collection.{Iterator, mutable}
 
-trait RawUpdate
-
-/*Todo: All the edge representations for updates have the property field. It makes sense to use subclasses with and
-   without edge property, in order to save some memory. The resulting dynamic dispatch should be cheap -- at most bimorphic,
-   which doesn't prevent inlining, and well-predicted since edge properties are so rarely used.*/
-class AddEdgeUnprocessed(val src: DNodeOrNode, val dst: DNodeOrNode, val edgeKind: Short, val property: Any) extends RawUpdate
-
-class AddUnsafeHalfEdge(val src: DNodeOrNode, val dst: DNodeOrNode, val edgeKind: Short, val inout: Byte, val property: Any)
-    extends RawUpdate
-
-class RemoveEdge(val edge: Edge)                         extends RawUpdate
-class SetEdgeProperty(val edge: Edge, val property: Any) extends RawUpdate
-
-class DelNode(val node: GNode) extends RawUpdate
-
-class SetNodeProperty(val node: GNode, val propertyKind: Int, val property: Any) extends RawUpdate
-
 class DiffGraphBuilder {
   var buffer = mutable.ArrayDeque[RawUpdate]()
 
@@ -78,3 +61,20 @@ class DiffGraphBuilder {
     }
   }
 }
+
+private[odb2] trait RawUpdate
+
+/*Todo: All the edge representations for updates have the property field. It makes sense to use subclasses with and
+   without edge property, in order to save some memory. The resulting dynamic dispatch should be cheap -- at most bimorphic,
+   which doesn't prevent inlining, and well-predicted since edge properties are so rarely used.*/
+private[odb2] class AddEdgeUnprocessed(val src: DNodeOrNode, val dst: DNodeOrNode, val edgeKind: Short, val property: Any) extends RawUpdate
+
+private[odb2] class AddUnsafeHalfEdge(val src: DNodeOrNode, val dst: DNodeOrNode, val edgeKind: Short, val inout: Byte, val property: Any)
+    extends RawUpdate
+
+private[odb2] class RemoveEdge(val edge: Edge)                         extends RawUpdate
+private[odb2] class SetEdgeProperty(val edge: Edge, val property: Any) extends RawUpdate
+
+private[odb2] class DelNode(val node: GNode) extends RawUpdate
+
+private[odb2] class SetNodeProperty(val node: GNode, val propertyKind: Int, val property: Any) extends RawUpdate
