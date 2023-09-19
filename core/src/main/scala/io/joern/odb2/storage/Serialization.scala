775ab03e795c193375b183ed647cfc418e3afddb
Michael Pollmeier <michael@michaelpollmeier.com>
2023-09-19 08:59:00 +0200

    move StorageType and Keys into storage package object

--- core/src/main/scala/io/joern/odb2/storage/Serialization.scala
+++ core/src/main/scala/io/joern/odb2/storage/Serialization.scala
@@ -12,41 +12,6 @@ import java.nio.{ByteBuffer, ByteOrder}
 import java.util.concurrent.atomic.AtomicLong
 import scala.collection.mutable
 
-object StorageType {
-  val Bool   = "bool"
-  val Byte   = "byte"
-  val Short  = "short"
-  val Int    = "int"
-  val Long   = "long"
-  val Ref    = "ref"
-  val String = "string"
-  val Double = "double"
-  val Float  = "float"
-}
-
-object Keys {
-  val Type               = "type"
-  val StartOffset        = "startOffset"
-  val CompressedLength   = "compressedLength"
-  val DecompressedLength = "decompressedLength"
-  val NodeLabel          = "nodeLabel"
-  val PropertyLabel      = "propertyLabel"
-  val Quantity           = "qty"
-  val Property           = "property"
-  val NNodes             = "nnodes"
-  val Deletions          = "deletions"
-  val EdgeLabel          = "edgeLabel"
-  val InOut              = "inout"
-  val Neighbors          = "neighbors"
-  val Version            = "version"
-  val Nodes              = "nodes"
-  val Edges              = "edges"
-  val Properties         = "properties"
-  val StringPoolLength   = "stringPoolLength"
-  val StringPoolBytes    = "stringPoolBytes"
-  val Header             = 0xdeadbeefdeadbeefL
-}
-
 object Serialization {
 
   def writeGraph(g: Graph, filename: String = "/tmp/foo.fg"): Unit = {
