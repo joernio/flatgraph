78f5ca830f851ab2d94461c35b6e8e85acb22849
Michael Pollmeier <michael@michaelpollmeier.com>
2023-09-26 12:18:30 +0200

    Traversal: always return Iterator
    * force users into the lazy model, as soon as they use our api

--- core/src/main/scala/io/joern/odb2/Traversal.scala
+++ core/src/main/scala/io/joern/odb2/Traversal.scala
@@ -4,7 +4,7 @@ import scala.reflect.ClassTag
 
 object Traversal {
 
-  extension[A, Iter[A] <: IterableOnce[A]] (iter: Iter[A]) {
+  extension[A] (iter: IterableOnce[A]) {
 
     /** casts all elements to given type
      * note: this can lead to casting errors
@@ -12,13 +12,13 @@ object Traversal {
      * @see {{{collectAll}}} as a safe alternative
      */
     // TODO reimplement  @Doc(info = "casts all elements to given type")
-    def cast[B]: Iter[B] =
-      iter.asInstanceOf[Iter[B]]
+    def cast[B]: Iterator[B] =
+      iter.iterator.asInstanceOf[Iterator[B]]
 
     /** collects all elements of the given class (beware of type-erasure) */
     // TODO reimplement   @Doc(info = "collects all elements of the provided class (beware of type-erasure)")
-    def collectAll[B](implicit ev: ClassTag[B]): Iter[B] =
-      iter.iterator.filter(ev.runtimeClass.isInstance).asInstanceOf[Iter[B]]
+    def collectAll[B](implicit ev: ClassTag[B]): Iterator[B] =
+      iter.iterator.filter(ev.runtimeClass.isInstance).asInstanceOf[Iterator[B]]
   }
 
 }
