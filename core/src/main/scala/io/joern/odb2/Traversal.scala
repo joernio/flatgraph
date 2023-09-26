e649efc221d191cd5ab76c8517527fe4de5038c5
Michael Pollmeier <michael@michaelpollmeier.com>
2023-09-26 12:44:42 +0200

    define everything on Iterator, but allow incoming usage on IterableOnce
    * as smooth as possible by using a given conversion

--- core/src/main/scala/io/joern/odb2/Traversal.scala
+++ core/src/main/scala/io/joern/odb2/Traversal.scala
@@ -4,7 +4,11 @@ import scala.reflect.ClassTag
 
 object Traversal {
 
-  extension[A] (iter: IterableOnce[A]) {
+  given Conversion[IterableOnce[?], Iterator[?]] =
+    iterable => iterable.iterator
+
+
+  extension[A] (iterator: Iterator[A]) {
 
     /** casts all elements to given type
      * note: this can lead to casting errors
@@ -13,12 +17,12 @@ object Traversal {
      */
     // TODO reimplement  @Doc(info = "casts all elements to given type")
     def cast[B]: Iterator[B] =
-      iter.iterator.asInstanceOf[Iterator[B]]
+      iterator.asInstanceOf[Iterator[B]]
 
     /** collects all elements of the given class (beware of type-erasure) */
     // TODO reimplement   @Doc(info = "collects all elements of the provided class (beware of type-erasure)")
     def collectAll[B](implicit ev: ClassTag[B]): Iterator[B] =
-      iter.iterator.filter(ev.runtimeClass.isInstance).asInstanceOf[Iterator[B]]
+      iterator.filter(ev.runtimeClass.isInstance).asInstanceOf[Iterator[B]]
   }
 
 }
