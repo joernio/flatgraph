bd64703bfb8bdc6cd18b2f2de22ffb1c071faddf
Michael Pollmeier <michael@michaelpollmeier.com>
2023-09-26 09:16:45 +0200

    decided for generic variant for now
    EdgeAccessors can themselves decide which collection type they want to use

---
+++ benchmarks/src/test/scala/io/joern/odb2/benchmark/EdgeAccessors.scala
@@ -0,0 +1,14 @@
+package io.joern.odb2.benchmark
+
+import io.shiftleft.codepropertygraph.generated.v2.nodes.*
+import io.joern.odb2.Traversal.*
+
+// TODO generate these by codegen, then remove this file
+object EdgeAccessors {
+  extension (call: Call) {
+    def argument: Iterator[Expression] = {
+      call._argumentOut.iterator.collectAll[Expression]
+    }
+  }
+
+}
--- core/src/main/scala/io/joern/odb2/Traversal.scala
+++ core/src/main/scala/io/joern/odb2/Traversal.scala
@@ -3,22 +3,6 @@ package io.joern.odb2
 import scala.reflect.ClassTag
 
 object Traversal {
-//    extension[A] (iter: Iterator[A]) {
-//
-//      /** casts all elements to given type
-//       * note: this can lead to casting errors
-//       * @see {{{collectAll}}} as a safe alternative
-//       */
-//      // TODO reimplement  @Doc(info = "casts all elements to given type")
-//      def cast[B]: Iterator[B] =
-//        iter.asInstanceOf[Iterator[B]]
-//
-//      /** collects all elements of the given class (beware of type-erasure) */
-//      // TODO reimplement   @Doc(info = "collects all elements of the provided class (beware of type-erasure)")
-//      def collectAll[B](implicit ev: ClassTag[B]): Iterator[B] =
-//        iter.filter(ev.runtimeClass.isInstance).asInstanceOf[Iterator[B]]
-//
-//    }
 
   extension[A, Iter[A] <: IterableOnce[A]] (iter: Iter[A]) {
 
@@ -36,22 +20,5 @@ object Traversal {
     def collectAll[B](implicit ev: ClassTag[B]): Iter[B] =
       iter.iterator.filter(ev.runtimeClass.isInstance).asInstanceOf[Iter[B]]
   }
-
-//  extension[A] (iter: Iterator[A]) {
-//
-//    /** casts all elements to given type
-//     * note: this can lead to casting errors
-//     * @see {{{collectAll}}} as a safe alternative
-//     */
-//    // TODO reimplement  @Doc(info = "casts all elements to given type")
-//    def cast[B]: Iterator[B] =
-//      iter.asInstanceOf[Iterator[B]]
-//
-//    /** collects all elements of the given class (beware of type-erasure) */
-//    // TODO reimplement   @Doc(info = "collects all elements of the provided class (beware of type-erasure)")
-//    def collectAll[B](implicit ev: ClassTag[B]): Iterator[B] =
-//      iter.filter(ev.runtimeClass.isInstance).asInstanceOf[Iterator[B]]
-//
-//  }
 
 }
