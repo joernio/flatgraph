2cd19a57d6a4aeb45b6ea6f893d8fb2e4e9b764c
Michael Pollmeier <michael@michaelpollmeier.com>
2023-11-06 20:54:12 +0100

    node.id: synthetically combine from nodeKind and seq (#93)
    * node.id: synthetically combine from nodeKind and seq
    
    * pr comment

--- core/src/main/java/io/joern/odb2/GNode.java
+++ core/src/main/java/io/joern/odb2/GNode.java
@@ -18,6 +18,17 @@ public class GNode implements DNodeOrNode {
         return this._seqId;
     }
 
+    /** 
+     * Combines nodeKind and seq into a unique id
+     * Mostly for backwards compatibility with overflowdb v1 where nodes have a `id: Long`
+     */
+    final public long id() {
+        // Promote nodeKind (short) to a higher area using left shift and combine with int
+        // note: masking the seq with 0x00000000ffffffffL to ensure that we treat is as an unsigned 32-bit integer - 
+        // shouldn't really be necessary, but better safe than sorry
+        return ((long) nodeKind << 32) | (seq() & 0x00000000ffffffffL);
+    }
+
     public GNode(Graph graph, short nodeKind, int seqId) {
         this.nodeKind = nodeKind;
         this._seqId = seqId;
