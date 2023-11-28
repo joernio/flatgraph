06d1f0576a0214dc4e6afa3022a526e432628649
Michael Pollmeier <michael@michaelpollmeier.com>
2023-11-28 12:55:58 +0100

    remove ClassTag where it's not required
    is it required???

--- core/src/main/scala/flatgraph/Accessors.scala
+++ core/src/main/scala/flatgraph/Accessors.scala
@@ -4,8 +4,6 @@ import flatgraph.Edge.Direction
 import flatgraph.Edge.Direction.{Incoming, Outgoing}
 import flatgraph.misc.{ISeq, MultiDictIndex}
 
-import scala.reflect.ClassTag
-
 object Accessors {
 
   def getEdgesOut(node: GNode, edgeKind: Int): IndexedSeq[Edge] = {
@@ -132,9 +130,7 @@ object Accessors {
     vals(qty(seq))
   }
 
-  def getNodePropertyOption[@specialized T](graph: Graph, nodeKind: Int, propertyKind: Int, seq: Int)(implicit
-    evidence: ClassTag[T]
-  ): Option[T] = {
+  def getNodePropertyOption[@specialized T](graph: Graph, nodeKind: Int, propertyKind: Int, seq: Int): Option[T] = {
     val pos = graph.schema.propertyOffsetArrayIndex(nodeKind, propertyKind)
     val qty = graph.properties(pos).asInstanceOf[Array[Int]]
     if (qty == null || seq + 1 >= qty.length || qty(seq) == qty(seq + 1)) return None
@@ -143,9 +139,7 @@ object Accessors {
     Some(vals(qty(seq)))
   }
 
-  def getNodePropertyMulti[@specialized T](graph: Graph, nodeKind: Int, propertyKind: Int, seq: Int)(implicit
-    evidence: ClassTag[T]
-  ): ISeq[T] = {
+  def getNodePropertyMulti[@specialized T](graph: Graph, nodeKind: Int, propertyKind: Int, seq: Int): ISeq[T] = {
     val pos = graph.schema.propertyOffsetArrayIndex(nodeKind, propertyKind)
     val qty = graph.properties(pos).asInstanceOf[Array[Int]]
     if (qty == null || seq + 1 >= qty.length) return ISeq.empty
