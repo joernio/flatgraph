9525f207696195afd08a5b96c8280c94b1e15149
Michael Pollmeier <michael@michaelpollmeier.com>
2024-02-13 19:43:23 +0100

    adjust column width dynamically by line length

--- core/src/test/scala/flatgraph/help/TableTests.scala
+++ core/src/test/scala/flatgraph/help/TableTests.scala
@@ -7,16 +7,16 @@ import flatgraph.help.Table.AvailableWidthProvider
 class TableTests extends AnyWordSpec {
 
   "render a nice generic table" in {
-    val table = Table(Seq("column a", "column b"), Seq(Seq("abc 1", "bde 1"), Seq("abc 2", "bde 2")))
+    val table = Table(Seq("column a", "column b"), Seq(Seq("abc 1", "bde 1"), Seq("abc 2", "bde 2 - little more content here")))
 
-    implicit val availableWidthProvider: AvailableWidthProvider = new Table.ConstantWidth(100)
+    implicit val availableWidthProvider: AvailableWidthProvider = new Table.ConstantWidth(50)
     table.render.trim shouldBe
-      """┌─────────────────────────────────────────────────┬────────────────────────────────────────────────┐
-        |│column a                                         │column b                                        │
-        |├─────────────────────────────────────────────────┼────────────────────────────────────────────────┤
-        |│abc 1                                            │bde 1                                           │
-        |│abc 2                                            │bde 2                                           │
-        |└─────────────────────────────────────────────────┴────────────────────────────────────────────────┘
+      """┌────────┬────────────────────────────────┐
+        |│column a│column b                        │
+        |├────────┼────────────────────────────────┤
+        |│abc 1   │bde 1                           │
+        |│abc 2   │bde 2 - little more content here│
+        |└────────┴────────────────────────────────┘
         |""".stripMargin.trim
   }
 
@@ -26,34 +26,34 @@ class TableTests extends AnyWordSpec {
       Seq(
         Seq(
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et" +
-            " dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip " +
-            "ex ea commodo consequat."
+            " dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip"
         )
       )
     )
 
-    var currentTerminalWidth                                    = 80 // think "looking up current value from an actual terminal"
+    var currentTerminalWidth                                    = 50 // think "looking up current value from an actual terminal"
     implicit val availableWidthProvider: AvailableWidthProvider = () => currentTerminalWidth
 
     table.render.trim shouldBe
-      """┌──────────────────────────────────────────────────────────────────────────────┐
-        |│lorem ipsum                                                                   │
-        |├──────────────────────────────────────────────────────────────────────────────┤
-        |│Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor│
-        |│incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis    │
-        |│nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. │
-        |└──────────────────────────────────────────────────────────────────────────────┘
+      """┌───────────────────────────────────────────────────────┐
+        |│lorem ipsum                                            │
+        |├───────────────────────────────────────────────────────┤
+        |│Lorem ipsum dolor sit amet, consectetur adipiscing     │
+        |│elit, sed do eiusmod tempor incididunt ut labore et    │
+        |│dolore magna aliqua. Ut enim ad minim veniam, quis     │
+        |│nostrud exercitation ullamco laboris nisi ut aliquip   │
+        |└───────────────────────────────────────────────────────┘
         |""".stripMargin.trim
 
     currentTerminalWidth = 100 // emulating: terminal size has changed
     table.render.trim shouldBe
-      """┌──────────────────────────────────────────────────────────────────────────────────────────────────┐
-        |│lorem ipsum                                                                                       │
-        |├──────────────────────────────────────────────────────────────────────────────────────────────────┤
-        |│Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut      │
-        |│labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris │
-        |│nisi ut aliquip ex ea commodo consequat.                                                          │
-        |└──────────────────────────────────────────────────────────────────────────────────────────────────┘
+      """┌───────────────────────────────────────────────────────────────────────────────────────────────┐
+        |│lorem ipsum                                                                                    │
+        |├───────────────────────────────────────────────────────────────────────────────────────────────┤
+        |│Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut   │
+        |│labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco      │
+        |│laboris nisi ut aliquip                                                                        │
+        |└───────────────────────────────────────────────────────────────────────────────────────────────┘
         |""".stripMargin.trim
   }
 
