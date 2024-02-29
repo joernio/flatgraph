a6e6b0cb3674758618486610e4f2930c82c6fed9
Michael Pollmeier <michael@michaelpollmeier.com>
2024-02-29 15:53:45 +0100

    reimplement DotTests

--- formats/src/test/scala/flatgraph/formats/dot/DotTests.scala
+++ formats/src/test/scala/flatgraph/formats/dot/DotTests.scala
@@ -1,43 +1,42 @@
 package flatgraph.formats.dot
 
-import better.files._
-import org.scalatest.matchers.should.Matchers._
+import better.files.*
+import flatgraph.*
+import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
-import flatgraph.formats.graphml.{GraphMLExporter, GraphMLImporter}
-import flatgraph.testdomains.simple.{FunkyList, SimpleDomain, TestEdge, TestNode}
-import flatgraph.util.DiffTool
-
-import scala.jdk.CollectionConverters.{CollectionHasAsScala, IterableHasAsJava}
 
 class DotTests extends AnyWordSpec {
 
   "Exporter should export valid dot" in {
-    val graph = SimpleDomain.newGraph()
-
-    val node2 = graph.addNode(2, TestNode.LABEL, TestNode.STRING_PROPERTY, """string"Prop2\""")
-    val node3 = graph.addNode(3, TestNode.LABEL, TestNode.INT_PROPERTY, 13)
-
-    // only allows values defined in FunkyList.funkyWords
-    val funkyList = new FunkyList()
-    funkyList.add("apoplectic")
-    funkyList.add("bucolic")
-    val node1 = graph.addNode(
-      1,
-      TestNode.LABEL,
-      TestNode.INT_PROPERTY,
-      11,
-      TestNode.STRING_PROPERTY,
-      "<stringProp1>",
-      TestNode.STRING_LIST_PROPERTY,
-      List("stringListProp1a", "stringList\\Prop1b").asJava,
-      TestNode.FUNKY_LIST_PROPERTY,
-      funkyList,
-      TestNode.INT_LIST_PROPERTY,
-      List(21, 31, 41).asJava
+    val testNodeLabel = "testNode"
+    val testEdgeLabel = "testEdge"
+    val schema = new FreeSchema(
+      nodeLabels = Array(testNodeLabel),
+      edgeLabels = Array(testEdgeLabel),
+      propertyLabels = Array("StringProperty", "StringListProperty", "IntProperty", "IntListProperty"),
+      nodePropertyPrototypes = Array(Array.empty[String], Array.empty[String], Array.emptyIntArray, Array.emptyIntArray),
+      edgePropertyPrototypes = Array(Array.emptyLongArray),
     )
 
-    node1.addEdge(TestEdge.LABEL, node2, TestEdge.LONG_PROPERTY, Long.MaxValue)
-    node2.addEdge(TestEdge.LABEL, node3)
+    val graph = new Graph(schema)
+    val v0 = new GenericDNode(0)
+    val v1 = new GenericDNode(0)
+    val v2 = new GenericDNode(0)
+
+    DiffGraphApplier.applyDiff(graph, new DiffGraphBuilder(schema)
+      ._addEdge(v0, v1, 0, Long.MaxValue)
+      ._addEdge(v1, v2, 0))
+
+    DiffGraphApplier.applyDiff(
+      graph,
+      new DiffGraphBuilder(schema)
+        .setNodeProperty(v0.storedRef.get, "IntProperty", 11)
+        .setNodeProperty(v0.storedRef.get, "StringProperty", "<stringProp1>")
+        .setNodeProperty(v0.storedRef.get, "StringListProperty", List("stringListProp1a", "stringList\\Prop1b"))
+        .setNodeProperty(v0.storedRef.get, "IntListProperty", List(21, 31, 41))
+        .setNodeProperty(v1.storedRef.get, "StringProperty", """string"Prop2\""")
+        .setNodeProperty(v2.storedRef.get, "IntProperty", 13)
+    )
 
     File.usingTemporaryDirectory(getClass.getName) { exportRootDirectory =>
       val exportResult = DotExporter.runExport(graph, exportRootDirectory.pathAsString)
@@ -47,14 +46,15 @@ class DotTests extends AnyWordSpec {
 
       val result = better.files.File(exportedFile).contentAsString.trim
       withClue(s"actual result was: `$result`") {
-        result shouldBe
+        result.trim shouldBe
           """digraph {
-             |  2 [label=testNode StringProperty="string\"Prop2\\"]
-             |  3 [label=testNode StringProperty="DEFAULT_STRING_VALUE" IntProperty=13]
-             |  1 [label=testNode FunkyListProperty="apoplectic;bucolic" StringProperty="<stringProp1>" StringListProperty="stringListProp1a;stringList\Prop1b" IntProperty=11 IntListProperty="21;31;41"]
-             |  2 -> 3 [label=testEdge longProperty=-99]
-             |  1 -> 2 [label=testEdge longProperty=9223372036854775807]
-             |}""".stripMargin.trim
+            |  0[label=testNode StringProperty="<stringProp1>" StringListProperty="stringListProp1a;stringList\Prop1b" IntProperty="11" IntListProperty="21;31;41"]
+            |  1[label=testNode StringProperty="string"Prop2\"]
+            |  2[label=testNode IntProperty="13"]
+            |  0 -> 1 [label=testEdge property=9223372036854775807]
+            |  1 -> 2 [label=testEdge property=0]
+            |}
+            |""".stripMargin.trim
       }
     }
   }
