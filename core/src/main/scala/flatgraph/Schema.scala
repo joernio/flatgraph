b220521ae0e7c11fc2f93a3dc7badecf90caf7fb
Michael Pollmeier <michael@michaelpollmeier.com>
2024-03-06 12:54:42 +0100

    cleanup

--- core/src/main/scala/flatgraph/Schema.scala
+++ core/src/main/scala/flatgraph/Schema.scala
@@ -8,11 +8,6 @@ final class DefaultValue(val default: Any)
 
 object Schema {
   val UndefinedKind: Short = -1
-
-  // TODO drop me
-  val xxxx = new Array[FormalQtyType.FormalQuantity | FormalQtyType.FormalType](1111)
-  xxxx(0) = FormalQtyType.QtyOne
-  xxxx(1) = FormalQtyType.IntType
 }
 
 object FormalQtyType {
