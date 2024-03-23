37514cbd64066bdf59b7581ccfa8e0707ce7bf89
Bernhard <bernhard@shiftleft.io>
2024-03-23 21:29:45 +0100

    make setEdgeProperty also cow

--- core/src/main/scala/flatgraph/DiffGraphApplier.scala
+++ core/src/main/scala/flatgraph/DiffGraphApplier.scala
@@ -408,7 +408,7 @@ private[flatgraph] class DiffGraphApplier(graph: Graph, diff: DiffGraphBuilder) 
     val oldQty = graph.neighbors(pos).asInstanceOf[Array[Int]]
     val edgeProp = graph.neighbors(pos + 2) match {
       case _: DefaultValue => graph.schema.allocateEdgeProperty(nodeKind, direction, edgeKind, size)
-      case other           => other
+      case other: Array[_] => other.clone() // this is not strictly necessary, but everything else is copy-on-write
     }
     val propview = mutable.ArraySeq.make(edgeProp.asInstanceOf[Array[_]]).asInstanceOf[mutable.ArraySeq[Any]]
     // this will fail if the edge doesn't support properties. todo: better error message
