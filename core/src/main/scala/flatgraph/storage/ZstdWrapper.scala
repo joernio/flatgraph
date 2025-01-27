57341e81e70a0ec83770b2f4f17c3fc4b324419a
Michael Pollmeier <michael@michaelpollmeier.com>
2025-01-27 12:56:29 +0100

    Update core/src/main/scala/flatgraph/storage/ZstdWrapper.scala

--- core/src/main/scala/flatgraph/storage/ZstdWrapper.scala
+++ core/src/main/scala/flatgraph/storage/ZstdWrapper.scala
@@ -66,7 +66,8 @@ object ZstdWrapper {
           ctx.decompress(bytes, len).order(ByteOrder.LITTLE_ENDIAN)
         } finally {
           this.synchronized {
-            if (this.closed) ctx.close() else decompressCtxs.append(ctx)
+            if (this.closed) ctx.close() 
+            else decompressCtxs.append(ctx)
           }
         }
       }
