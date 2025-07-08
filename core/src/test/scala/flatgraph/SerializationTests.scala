aef8189a1f30efe717c3cf2cfdbf3a662ca6e024
Michael Pollmeier <michael@michaelpollmeier.com>
2025-07-08 14:35:59 +0200

    Deserialization: verify manifest size
    re https://github.com/joernio/flatgraph/security/advisories/GHSA-jqmx-3x2p-69vh
    
    n.b. it is not our goal to have a "safe" file format, and we need to
    document that properly. But adding some more straightforward checks
    doesn't harm.

--- core/src/test/scala/flatgraph/SerializationTests.scala
+++ core/src/test/scala/flatgraph/SerializationTests.scala
@@ -1,11 +1,16 @@
 package flatgraph
 
 import flatgraph.misc.DebugDump.debugDump
+import flatgraph.storage.Deserialization.DeserializationException
 import flatgraph.storage.{Deserialization, Serialization}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-import java.nio.file.Files
+import java.nio.file.{Files, Path}
+import java.io.RandomAccessFile
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import scala.util.Using
 
 class SerializationTests extends AnyWordSpec with Matchers {
 
@@ -38,6 +43,45 @@ class SerializationTests extends AnyWordSpec with Matchers {
     val deserialized = Deserialization.readGraph(storagePath, Option(graph.schema))
     val newDump      = debugDump(deserialized)
     originalDump shouldBe newDump
+  }
+
+  /* show that we're no longer vulnerable to the denial of service issue filed here:
+   * https://github.com/joernio/flatgraph/security/advisories/GHSA-jqmx-3x2p-69vh
+   */
+  "is no longer vulnerable to manifest size attack" in {
+    val schema = TestSchema.make(1, 0)
+    val graph  = Graph(schema)
+    val diff   = DiffGraphBuilder(schema).addNode(new GenericDNode(0))
+    DiffGraphApplier.applyDiff(graph, diff)
+
+    val storagePath = Files.createTempFile(s"flatgraph-${getClass.getSimpleName}", "fg")
+    Serialization.writeGraph(graph, storagePath)
+    patchFile(storagePath)
+
+    // when the vulnerability was reported, the following line raised a:
+    // `java.lang.OutOfMemoryError: Requested array size exceeds VM limit`
+    intercept[DeserializationException] {
+      Deserialization.readGraph(storagePath, Option(graph.schema))
+    }.getMessage should include("corrupt file: manifest size")
+  }
+
+  /** manipulate file as detailed in https: //github.com/joernio/flatgraph/security/advisories/GHSA-jqmx-3x2p-69vh */
+  private def patchFile(path: Path): Unit = {
+    Using.resource(new RandomAccessFile(path.toFile, "rw")) { file =>
+      // Seek to end and get file size
+      file.seek(file.length())
+      val fileSize = file.getFilePointer
+
+      // Calculate malicious offset
+      val maliciousOffset = fileSize - 2147483647L
+
+      // Seek to position 8 and write the offset as little-endian long
+      file.seek(8)
+      val buffer = ByteBuffer.allocate(8)
+      buffer.order(ByteOrder.LITTLE_ENDIAN)
+      buffer.putLong(maliciousOffset)
+      file.write(buffer.array())
+    }
   }
 
 }
