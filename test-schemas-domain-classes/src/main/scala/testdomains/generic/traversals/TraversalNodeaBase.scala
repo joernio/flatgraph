86cb2899a764733f690c4424140ffb1b6e9bf3e8
Louis van der Walt <13525489+SuperUserDone@users.noreply.github.com>
2026-05-26 09:16:06 +0200

    Add Domain Classes for tests (#358)

--- test-schemas-domain-classes/src/main/scala/testdomains/generic/traversals/TraversalNodeaBase.scala
+++ test-schemas-domain-classes/src/main/scala/testdomains/generic/traversals/TraversalNodeaBase.scala
@@ -18,6 +18,11 @@ final class TraversalNodeaBase[NodeType <: nodes.NodeABase](val traversal: Itera
   def intMandatory(value: Int): Iterator[NodeType] =
     traversal.filter { _.intMandatory == value }
 
+  /** Traverse to nodes where the intMandatory equals the given `value`, or no results if `value` is None
+    */
+  def intMandatory(value: Option[Int]): Iterator[NodeType] =
+    value match { case Some(_val) => intMandatory(_val); case None => Iterator.empty }
+
   /** Traverse to nodes where the intMandatory equals at least one of the given `values`
     */
   def intMandatory(values: Int*): Iterator[NodeType] = {
@@ -67,6 +72,17 @@ final class TraversalNodeaBase[NodeType <: nodes.NodeABase](val traversal: Itera
     traversal.filter { node =>
       val tmp = node.intOptional; tmp.isDefined && tmp.get == value
     }
+
+  /** Traverse to nodes where the intOptional equals the given `value`. If `value` is None, only nodes where intOptional is not set are
+    * included.
+    */
+  def intOptional(value: Option[Int]): Iterator[NodeType] =
+    value match { case Some(_val) => intOptional(_val); case None => traversal.filter { node => node.intOptional.isEmpty } }
+
+  /** Traverse to nodes where the intOptional equals the given `value`, or no results if `value` is None.
+    */
+  def intOptionalIfPresent(value: Option[Int]): Iterator[NodeType] =
+    value match { case Some(_val) => intOptional(_val); case None => Iterator.empty }
 
   /** Traverse to nodes where the intOptional equals at least one of the given `values`
     */
